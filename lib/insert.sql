43334229a9226ee8c68bd2393b858d247e6cafdf
Derek Mercedes <mercedes.derek@gmail.com>
2023-12-15 12:05:22 -0700

    tests passed

---
+++ lib/insert.sql
@@ -0,0 +1,23 @@
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES
+("Mr. Chocolate", 3, "m", "black", "chill", 1);
+
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES
+("Rowdy", 2, "f", "white", "rowdy", 1);
+
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES
+("Tabitha", 5, "f", "green", "trippy", 1);
+
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES
+("Sergeant Brown", 10, "m", "Yeller", "Ded", 0);
+
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES
+("Melissa", 5, "f", "Blue", "Icy", 1);
+
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES
+("Grinch", 1, "m", "green", "grumpy", 1);
+
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES
+("Wendy", 6, "f", "pink", "adventurous", 1);
+
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES
+(NULL, NULL, "m", "Brown", NULL, 0);
